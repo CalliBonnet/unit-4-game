e882be6c9263aa3d8cbbc95df94070388e653492
Calli Bonnet <calli@paradox.ai>
2019-09-12 18:26:09 -0700

    updated my images

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,56 +1,59 @@
 
-<!-- This is where my js cosde will go  -->
+$(document).ready(function(){
+
+let goalNumber = Math.floor(Math.random() * 101 + 21); 
 
 
-    // This is the number that the user must guess 
-    var goalNumber = 50;
-    var totalScore = 0; 
-    var wins = 0; 
-    var lose = 0; 
+$("#user-guess-number").text(goalNumber); 
 
-    $("#user-guess-number").text(goalNumber);
-
-    // This is calulating the user's score 
-    var count = 0;
-
-    // These are the numbers that each image can have 
-    var numberOptions = [7, 10, 4, 20]; 
+let btn1 = Math.floor(Math.random() * 11 + 1); 
 
 
-    // This is gettnig the images on the screen 4 times 
-    for (var i = 0; i < numberOptions.length; i++) {
-        var imageOfStan = $("<img>"); 
-        imageOfStan.addClass("image-Stan"); 
-        imageOfStan.attr("src", "./assests/images/Stan-In-The-Wind.jpg"); 
-        imageOfStan.attr("data-stanvalue", numberOptions[i]); 
-        $("#Stanley-Images").append(imageOfStan); 
+var totalScore = 0;
+var wins = 0;
+var lose = 0;
+var displayWins = $("user-wins"); 
+var displayLoses = $("user-lose"); 
+var displayUserScore = $("user-socre"); 
+
+userWinScore.text(wins); 
+displayLoses.text(lose); 
+displayUserScore.text(totalScore); 
+
+
+
+
+
+$("#user-guess-number").text(goalNumber);
+
+// This is calulating the user's score 
+var count = 0;
+
+// These are the numbers that each image can have 
+var numberOptions = [7, 10, 4, 20];
+
+
+
+//this is the code that counts the values and determines when you win/lose (do this 4 times)
+$(".image-Stan").on("click", function () {
+
+    var stanValue = ($(this).attr("data-stanvalue"));
+    stanValue = parseInt(stanValue);
+    count += stanValue;
+
+    alert("Your new score is: " + count)
+
+    //This is the function that will create the Win and Lose Alerts if the user's score matches the guess number 
+    if (count === goalNumber) {
+
+        alert("You are Winner!");
+
     }
+    else if (count >= goalNumber) {
 
+        alert("You lose! So Sad!");
+    };
 
-    $(".image-Stan").on("click", function () {
-        
-        var stanValue = ($(this).attr("data-stanvalue")); 
-        stanValue = parseInt(stanValue); 
-        count += stanValue; 
+});
 
-        alert("Your new score is: " + count)
-
-        //This is the function that will create the Win and Lose Alerts if the user's score matches the guess number 
-        if (count === goalNumber) {
-
-            alert("You are Winner!");
-
-        }
-        else if (count >= goalNumber) {
-
-            alert("You lose! So Sad!");
-        }; 
-
-
-        document.getElementsByClassName("user-socre").textConent = totalScore; 
-        document.getElementsByClassName("user-wins").textConent = wins; 
-        document.getElementsByClassName("user-lose").textConent = lose; 
-    });
-
-
-
+}); 
