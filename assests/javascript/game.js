be971ec11a828cf9a3d837032ed271f83ac3b479
Calli Bonnet <calli@paradox.ai>
2019-09-12 18:53:57 -0700

    updating

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,59 +1,117 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-let goalNumber = Math.floor(Math.random() * 101 + 21); 
+    //++++++++++++++++++++++++++This is creating the random number ++++++++++++++++++++++++++
+    let goalNumber = Math.floor(Math.random() * 101 + 21);
 
+    $("#user-guess-number").text(goalNumber);
 
-$("#user-guess-number").text(goalNumber); 
+    let btn1 = Math.floor(Math.random() * 11 + 1);
+    let btn2 = Math.floor(Math.random() * 11 + 1);
+    let btn3 = Math.floor(Math.random() * 11 + 1);
+    let btn4 = Math.floor(Math.random() * 11 + 1);
 
-let btn1 = Math.floor(Math.random() * 11 + 1); 
+    var totalScore = 0;
+    var wins = 0;
+    var lose = 0;
+    var displayWins = $("user-wins");
+    var displayLoses = $("user-lose");
+    var displayUserScore = $("user-socre");
 
-
-var totalScore = 0;
-var wins = 0;
-var lose = 0;
-var displayWins = $("user-wins"); 
-var displayLoses = $("user-lose"); 
-var displayUserScore = $("user-socre"); 
-
-userWinScore.text(wins); 
-displayLoses.text(lose); 
-displayUserScore.text(totalScore); 
-
-
-
+    // These are the numbers that each image can have 
+    var numberOptions = [7, 10, 4, 20];
 
 
-$("#user-guess-number").text(goalNumber);
-
-// This is calulating the user's score 
-var count = 0;
-
-// These are the numbers that each image can have 
-var numberOptions = [7, 10, 4, 20];
+    displayWins.text(wins);
+    displayLoses.text(lose);
+    displayUserScore.text(totalScore);
 
 
 
-//this is the code that counts the values and determines when you win/lose (do this 4 times)
-$(".image-Stan").on("click", function () {
 
-    var stanValue = ($(this).attr("data-stanvalue"));
-    stanValue = parseInt(stanValue);
-    count += stanValue;
+    $("#user-guess-number").text(goalNumber);
 
-    alert("Your new score is: " + count)
 
-    //This is the function that will create the Win and Lose Alerts if the user's score matches the guess number 
-    if (count === goalNumber) {
+    //this is the code that counts the values and determines when you win/lose (do this 4 times)
+    $(".btn1").on("click", function () {
 
-        alert("You are Winner!");
+        var stanValue = ($(this).attr("data-stanvalue"));
+        stanValue = parseInt(stanValue);
+        totalScore += stanValue;
 
-    }
-    else if (count >= goalNumber) {
+        alert("Your new score is: " + totalScore)
 
-        alert("You lose! So Sad!");
-    };
+        //This is the function that will create the Win and Lose Alerts if the user's score matches the guess number 
+        if (totalScore === goalNumber) {
 
-});
+            alert("You are Winner!");
+
+        }
+        else if (totalScore >= goalNumber) {
+
+            alert("You lose! So Sad!");
+        };
+    });
+
+    $(".btn2").on("click", function () {
+
+        var stanValue = ($(this).attr("data-stanvalue"));
+        stanValue = parseInt(stanValue);
+        totalScore += stanValue;
+
+        alert("Your new score is: " + totalScore)
+
+        //This is the function that will create the Win and Lose Alerts if the user's score matches the guess number 
+        if (totalScore === goalNumber) {
+
+            alert("You are Winner!");
+
+        }
+        else if (totalScore >= goalNumber) {
+
+            alert("You lose! So Sad!");
+        };
+    });
+
+    $(".btn3").on("click", function () {
+
+        var stanValue = ($(this).attr("data-stanvalue"));
+        stanValue = parseInt(stanValue);
+        totalScore += stanValue;
+
+        alert("Your new score is: " + totalScore)
+
+        //This is the function that will create the Win and Lose Alerts if the user's score matches the guess number 
+        if (totalScore === goalNumber) {
+
+            alert("You are Winner!");
+
+        }
+        else if (totalScore >= goalNumber) {
+
+            alert("You lose! So Sad!");
+        };
+    });
+
+    $(".btn4").on("click", function () {
+
+        var stanValue = ($(this).attr("data-stanvalue"));
+        stanValue = parseInt(stanValue);
+        totalScore += stanValue;
+
+        alert("Your new score is: " + totalScore)
+
+        //This is the function that will create the Win and Lose Alerts if the user's score matches the guess number 
+        if (totalScore === goalNumber) {
+
+            alert("You are Winner!");
+
+        }
+        else if (totalScore >= goalNumber) {
+
+            alert("You lose! So Sad!");
+        };
+
+    });
 
 }); 
