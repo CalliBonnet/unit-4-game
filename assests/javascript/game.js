8f584e2c11b3586058061c4fb84f5a028ba2c2e7
Calli Bonnet <calli@paradox.ai>
2019-09-15 18:08:20 -0700

    updatig js

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,8 +1,23 @@
-//start 
-$(document).ready(function () {
+
+var randomResult;
+let lost;
+let win;
 
 
-    
+randomResult = Math.floor(Math.random() * 99 - 30); 
 
+$("#result").html('Random Result: '); 
 
-}); 
+//these are the crystals 
+for (var i = 0; i < 4; i++) {
+
+    var random = Math.floor(Math.random() * 12);
+
+    var crystal = $("<div>");
+    crystal.attr({
+        "class": 'crystal',
+        "data-number": random,
+    });
+
+    $(".crystals").append(crystal);
+}; 
