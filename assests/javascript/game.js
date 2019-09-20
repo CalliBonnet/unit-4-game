33533919fe4d1ef95e9c96eb71f5621b5c019a1c
Calli Bonnet <calli@paradox.ai>
2019-09-19 18:12:27 -0700

    fixed the crystals and added random numbers to the crystals

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -3,6 +3,8 @@ var randomResult;
 let lost;
 let win;
 
+//setters 
+//getters 
 
 
 // This is finding a random number for the user to guess to 
@@ -11,27 +13,24 @@ console.log(randomResult);
 
 $("#result").html('Random Result: ' + randomResult);
 
-
-
 //these are making the crystals appear on the screen 
 for (var i = 0; i < 4; i++) {
 
     var random = Math.floor(Math.random() * 11) + 1;
 
     var crystal = $("<div>");
-    crystal.attr({
-        "class": 'crystal',
-        "data-number": random
-    });
+        crystal.attr({
+            "class": 'crystal',
+            "data-number": random
+        });
 
     $(".crystals").append(crystal);
 };
 
 
-
-
 // this is when you click on a crystal, a number appears 
 $(".crystal").on('click', function () {
 
-    console.log($(this));
+    console.log($(this).attr('data-number
+    '));
 }); 
