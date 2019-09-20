c1e9df2a33a278efd0976d78286d6ce77cdc7cdf
Calli Bonnet <calli@paradox.ai>
2019-09-19 19:00:10 -0700

    added more js

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -4,14 +4,16 @@ let lost = 0;
 let win = 0;
 var pervious = 0;
 
-var resetandStartGame = function () {
-
+var resetAndStartGame = function () {
+    $(".crystals").empty(); 
 
     // This is finding a random number for the user to guess to 
     randomResult = Math.floor(Math.random() * 69) + 30;
     console.log(randomResult);
 
+    //this is populating the ramdon number that the user must guess to on the screen 
     $("#result").html('Random Result: ' + randomResult);
+
 
     //these are making the crystals appear on the screen 
     for (var i = 0; i < 4; i++) {
@@ -28,12 +30,15 @@ var resetandStartGame = function () {
     };
 }
 
-resetandStartGame(); 
+resetAndStartGame(); 
+
 
 
 
 // this is when you click on a crystal, a number appears 
-$(".crystal").on('click', function () {
+
+
+$(document).on('click', ".crystal", function () {
 
     var num = parseInt($(this).attr('data-number'));
 
@@ -41,17 +46,21 @@ $(".crystal").on('click', function () {
     console.log(pervious);
 
     if (pervious > randomResult) {
-        lost--;
+        lost++;
 
-        $("#Lost").html(lost);
-        console.log("you Lost!")
+        $("#Lost").html("Lost:" + lost);
+        pervious = 0; 
+
+        resetAndStartGame(); 
+
     }
     else if (pervious === randomResult) {
         win++;
 
-        $("#Win").html(win);
-
-        console.log("You win!");
+        $("#Win").html("Wins:" + win);
+        pervious = 0; 
+        
+        resetAndStartGame(); 
     };
 
 
